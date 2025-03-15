58aa0d4a9bd4bf1b48d23737cc1ed3360d072d7a
user49tbd <85646319+user49tbd@users.noreply.github.com>
2025-03-14 22:03:39 -0300

    Update securityConstants.java

--- securityTestPract/src/main/java/com/securityTestPract/securityTestPract/security/securityConstants.java
+++ securityTestPract/src/main/java/com/securityTestPract/securityTestPract/security/securityConstants.java
@@ -1,6 +1,6 @@
 package com.securityTestPract.securityTestPract.security;
 
 public class securityConstants {
-	public static final long JwtTime = 16000;
+	public static final long JwtTime = 60000;
 	public static final String JwtSecret = "6404cd84-9fca-41c7-a191-014fba89f44e";
 }
